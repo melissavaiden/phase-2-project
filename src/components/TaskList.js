3753d71595945c325f70711f1c60d8c42c368610
Melissa Vaiden <melissavaiden@gmail.com>
2022-07-30 18:45:57 -0400

    Adding times of tasks to task list

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,22 +3,23 @@ import NavBar from "./NavBar";
 import TaskCard from "./TaskCard";
 
 function TaskList({tasks}) {
-    const timeSpentCleaning = tasks.map((task) => {
-        const allTimes = []
+    const allTimes = []
+    tasks.map((task) => {
         if (task.name === 'Cleaning') {
             allTimes.push(Number(task.time))
-            console.log(allTimes)
-            allTimes.reduce((accumulator, currentValue) => {
-                return accumulator + currentValue
-            }, 0)
-        }
-    })
+        }})
+
+
+    let sumOfTimes = allTimes.reduce((accumulator, currentValue) => {
+            return accumulator + currentValue
+        }, 0)
+
 
     return (
         <div>
             <NavBar />
             <TaskCard tasks={tasks}/>
-            <div>Time Spent Cleaning:</div>
+            <div>Time Spent Cleaning: {sumOfTimes} minutes</div>
         </div>
     )
 }
